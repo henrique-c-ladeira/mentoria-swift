7971742855a2765c54c78120fe149fb366284235
Henrique <henrique.c.ladeira@gmail.com>
2023-08-02 21:09:21 -0300

    add ReactViewController

--- ios/RickAndMortyMentoria/Scenes/React/ReactViewController.swift
+++ ios/RickAndMortyMentoria/Scenes/React/ReactViewController.swift
@@ -6,5 +6,27 @@
 //
 
 import UIKit
+import React
 
-class ReactViewController: UIViewController {}
+
+class ReactViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")!
+          let mockData:NSDictionary = ["scores":
+              [
+                  ["name":"Alex", "value":"42"],
+                  ["name":"Joel", "value":"10"]
+              ]
+          ]
+
+          let rootView = RCTRootView(
+              bundleURL: jsCodeLocation,
+              moduleName: "RNHighScores",
+              initialProperties: mockData as [NSObject : AnyObject],
+              launchOptions: nil
+          )
+          view = rootView
+    }
+}
